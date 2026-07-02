3583bf157a310641e9ef13aa33c636671bb33a87
diogofragososouza <diogofragososouza@gmail.com>
2026-07-01 23:07:34 -0300

    Correção: Ajuste do base no vite.config e homepage no package.json

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: '/incarsads-web/',
+    base: '/incaradmin/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
